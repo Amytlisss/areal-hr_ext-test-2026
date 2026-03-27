686d8a84846822b6e3357eef6373b6755362835d
Amytlisss <pereginezee1980@gmail.com>
2026-03-27 16:09:56 +0300

    обновленные файлы в процессе созданий миграций

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,25 +3,25 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
+  imports:[
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService)=>({
         type: 'postgres',
         host: configService.get('DB_HOST'),
         port: configService.get('DB_PORT'),
         username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [__dirname+ '/**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: true,
       }),
-      inject: [ConfigService],
+      inject:[ConfigService],
     }),
     ],
 })
