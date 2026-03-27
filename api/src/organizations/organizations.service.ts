4db0a5a688b420ec102840c183876fb3687b192d
Amytlisss <pereginezee1980@gmail.com>
2026-03-27 20:46:50 +0300

    создание файла сервиса с круд организациям

---
+++ api/src/organizations/organizations.service.ts
@@ -0,0 +1,49 @@
+import{Injectable, NotFoundException} from '@nestjs/common';
+import{InjectRepository} from '@nestjs/typeorm';
+import{Repository, IsNull} from 'typeorm';
+import{Organization} from "./entities/organization.entity";
+import{CreateOrganizationDto} from "./dto/create-organization.dto";
+import{UpdateOrganizationDto} from './dto/update-organization.dto';
+
+@Injectable()
+export class OrganizationsService {
+  constructor(
+    @InjectRepository(Organization)
+    private organizationRepository:Repository<Organization>,
+  ) {}
+
+  async create(createOrganizationDto:CreateOrganizationDto):Promise<Organization> {
+    const organization=this.organizationRepository.create(createOrganizationDto);
+    return this.organizationRepository.save(organization);
+  }
+
+  async findAll(): Promise<Organization[]>{
+    return this.organizationRepository.find({
+      where:{deletedAt:IsNull()},
+      order:{ createdAt:"DESC"},
+    });
+  }
+
+  async findOne(id:string):Promise<Organization>{
+    const organization=await this.organizationRepository.findOne({
+      where:{id, deletedAt:IsNull()},
+    });
+    if (!organization){
+      throw new NotFoundException("Organization with ID ${id} not found");
+    }
+    return organization;
+  }
+
+  async update(id: string, updateOrganizationDto: UpdateOrganizationDto): Promise<Organization> {
+    const organization=await this.findOne(id);
+    Object.assign(organization, updateOrganizationDto);
+    organization.updatedAt=new Date();
+    return this.organizationRepository.save(organization);
+  }
+
+  async remove(id:string):Promise<void> {
+    const organization=await this.findOne(id);
+    organization.deletedAt=new Date();
+    await this.organizationRepository.save(organization);
+  }
+}
