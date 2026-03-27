a91d01ab5e51dd8c2dee830098edc1fff08b2886
Amytlisss <pereginezee1980@gmail.com>
2026-03-27 23:48:58 +0300

    создан обработчик запросов

---
+++ api/src/organizations/organizations.controller.ts
@@ -0,0 +1,35 @@
+import{Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus} from '@nestjs/common';
+import{OrganizationsService} from './organizations.service';
+import{CreateOrganizationDto} from './dto/create-organization.dto';
+import{UpdateOrganizationDto} from './dto/update-organization.dto';
+
+@Controller('organizations')
+export class OrganizationsController {
+  constructor(private readonly organizationsService: OrganizationsService) {}
+
+  @Post()
+  create(@Body() createOrganizationDto:CreateOrganizationDto) {
+    return this.organizationsService.create(createOrganizationDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.organizationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.organizationsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateOrganizationDto:UpdateOrganizationDto) {
+    return this.organizationsService.update(id, updateOrganizationDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.organizationsService.remove(id);
+  }
+}
