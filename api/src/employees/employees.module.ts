ea2bbc38373b61c62f71f8f48a97f0d3995e0c7d
Amytlisss <pereginezee1980@gmail.com>
2026-04-07 22:14:16 +0300

    исправлена ошибка2 про увольнение

--- api/src/employees/employees.module.ts
+++ api/src/employees/employees.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { Employee } from './entities/employee.entity';
+import { HrOperation } from '../hr-operations/entities/hr-operation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employee])],
+  imports: [TypeOrmModule.forFeature([Employee, HrOperation])],
   controllers: [EmployeesController],
   providers: [EmployeesService],
   exports: [EmployeesService],
